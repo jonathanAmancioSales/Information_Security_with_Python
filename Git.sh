6371ed0fe3c4ff2527cfffbdd4f5c1abc4a4a3e6
Jonathan Sales <jonathan.sales@fisica.ufc.br>
2021-03-08 21:03:35 -0300

    commit inicial

---
+++ Git.sh
@@ -0,0 +1,17 @@
+#sudo apt-get install git-all
+
+git --version
+
+#Configurando o Git:
+git config --global user.name "Jonathan Sales"
+git config --global user.email "jonathan.sales@fisica.ufc.br"
+
+#exibir todos os itens de configuração:
+git config --list
+
+git init
+
+#Criar arquivos
+#echo "..." > file.txt
+
+git add *
